8bd68feb3f85dc1fa67fe577461ecfc61fb32ebb
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2020-05-07 21:18:54 +0100

    remove

--- gimpformats/__init__.py
+++
@@ -1,8 +0,0 @@
-""" gimpformats
-
-Forked from https://github.com/TheHeadlessSourceMan/gimpFormats
-
-A pure python implementation of the GIMP XCF image format.
-
-Use this to interact with GIMP image formats
-"""
