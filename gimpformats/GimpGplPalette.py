b1a930c21ecdb2f4d5dd45ba0dd04972698316c4
Kieran BW <41634689+FredHappyface@users.noreply.github.com>
2020-10-29 22:54:32 +0000

    Update GimpGplPalette.py

--- gimpformats/GimpGplPalette.py
+++ gimpformats/GimpGplPalette.py
@@ -3,12 +3,13 @@
 Pure python implementation of the gimp gpl palette format
 """
 from __future__ import annotations
-import argparse
+from io import BytesIO
+from typing import Union
 
 class GimpGplPalette:
 	""" Pure python implementation of the gimp gpl palette format """
-	def __init__(self, fileName=None):
-		self.name = ''
+	def __init__(self, fileName: Union[BytesIO, str, None]=None):
+		self.name = ""
 		self.columns = 16
 		self.colors = []
 		self.colorNames = []
@@ -21,28 +22,28 @@ class GimpGplPalette:
 
 		:param fileName: can be a file name or a file-like object
 		"""
-		if hasattr(fileName, 'read'):
-			self.fileName = fileName.name
-			f = fileName
-		else:
+		if isinstance(fileName, str):
 			self.fileName = fileName
-			f = open(fileName, 'r')
-		data = f.read()
-		f.close()
+			file = open(fileName, "r")
+		else:
+			self.fileName = fileName.name
+			file = fileName
+		data = file.read()
+		file.close()
 		self.decode(data)
 
-	def decode(self, data):
+	def decode(self, data: str) -> None:
 		"""
 		decode a byte buffer
 
 		:param data: data buffer to decode
 		"""
-		data = [s.strip() for s in data.split('\n')]
-		if data[0] != "GIMP Palette":
-			raise Exception('File format error.  Magic value mismatch.')
-		self.name = data[1].split(':', 1)[-1].lstrip()
-		self.columns = int(data[2].split(':', 1)[-1].lstrip())
-		for line in data[3:]:
+		lines = [s.strip() for s in data.split("\n")]
+		if lines[0] != "GIMP Palette":
+			raise Exception("File format error.  Magic value mismatch.")
+		self.name = lines[1].split(":", 1)[-1].lstrip()
+		self.columns = int(lines[2].split(":", 1)[-1].lstrip())
+		for line in lines[3:]:
 			if len(line) < 1 or line[0] == "#": # Commented Line
 				continue
 			line = line.split(None, 4)
@@ -58,48 +59,43 @@ class GimpGplPalette:
 		""" encode to a raw data stream """
 		data = []
 		data.append("GIMP Palette")
-		data.append('Name: ' + str(self.name))
-		data.append('Columns: ' + str(self.columns))
+		data.append("Name: " + str(self.name))
+		data.append("Columns: " + str(self.columns))
 		data.append("#")
 		for i, color in enumerate(self.colors):
 			colorName = self.colorNames[i]
-			line = str(color[0]).rjust(3) + ' ' + str(color[1]).rjust(3) + ' ' + str(
+			line = str(color[0]).rjust(3) + " " + str(color[1]).rjust(3) + " " + str(
 			color[2]).rjust(3)
 			if colorName is not None:
-				line = line + '\t' + colorName
+				line = line + "\t" + colorName
 			data.append(line)
-		return ('\n'.join(data) + '\n').encode('utf-8')
+		return ("\n".join(data) + "\n").encode("utf-8")
 
-	def save(self, tofileName=None, toExtension=None):
+	def save(self, fileName: Union[str, BytesIO]):
 		""" save this gimp image to a file """
-		if toExtension is None:
-			if tofileName is not None:
-				toExtension = tofileName.rsplit('.', 1)
-				if len(toExtension) > 1:
-					toExtension = toExtension[-1]
-				else:
-					toExtension = None
-		if not hasattr(tofileName, 'write'):
-			f = open(tofileName, 'wb')
-		f.write(self.encode())
-		f.close()
+		if isinstance(fileName, str):
+			file = open(fileName, "wb")
+		else:
+			file = fileName
+		file.write(self.encode())
+		file.close()
 
-	def __repr__(self, indent=''):
+	def __repr__(self, indent: str=""):
 		""" Get a textual representation of this object """
 		ret = []
 		if self.fileName is not None:
-			ret.append('fileName: ' + self.fileName)
-		ret.append('Name: ' + str(self.name))
-		ret.append('Columns: ' + str(self.columns))
-		ret.append('Colors:')
+			ret.append("fileName: " + self.fileName)
+		ret.append("Name: " + str(self.name))
+		ret.append("Columns: " + str(self.columns))
+		ret.append("Colors:")
 		for i, color in enumerate(self.colors):
 			colorName = self.colorNames[i]
-			line = '(%d,%d,%d)' % color[0], color[1], color[2]
+			line = "(%d,%d,%d)" % color[0], color[1], color[2]
 			if colorName is not None:
-				line = line + ' ' + colorName
-		return '\n'.join(ret)
+				line = line + " " + colorName
+		return "\n".join(ret)
 
-	def __eq__(self, other):
+	def __eq__(self, other: GimpGplPalette):
 		""" perform a comparison """
 		if other.name != self.name:
 			return False
@@ -113,18 +109,3 @@ class GimpGplPalette:
 			if self.colorNames[i] != other.colorNames[i]:
 				return False
 		return True
-
-if __name__ == '__main__':
-	""" CLI Entry Point """
-	parser = argparse.ArgumentParser("GimpGplPalette.py")
-	parser.add_argument("xcfdocument", action="store",
-	help="xcf file to act on")
-	group = parser.add_mutually_exclusive_group()
-	group.add_argument("--dump", action="store_true",
-	help="dump info about this file")
-	args = parser.parse_args()
-
-	gimpGplPalette = GimpGplPalette(args.xcfdocument)
-
-	if args.dump:
-		print(gimpGplPalette)
