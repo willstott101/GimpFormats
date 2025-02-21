768d8fa69b7c5e7ae44652eaa52e0fbed6280505
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2025-02-21 20:48:59 +0000

    vbr_brush

--- tests/vbr_brush/test_vbr_brush.py
+++ tests/vbr_brush/test_vbr_brush.py
@@ -12,17 +12,23 @@ THISDIR = str(Path(__file__).resolve().parent)
 
 from gimpformats.GimpVbrBrush import GimpVbrBrush
 
-dut = GimpVbrBrush()
+project = GimpVbrBrush()
+
+
+def test_DiagonalStar() -> None:
+	"""Test diagonal star."""
+	project.load(f"{THISDIR}/Diagonal-Star-17.vbr")
+	# test round-trip compatibility
+	project.save(f"{THISDIR}/actualOutput_Diagonal-Star-17.vbr")
+	original = GimpVbrBrush(f"{THISDIR}/Diagonal-Star-17.vbr")
+	cmp = GimpVbrBrush(f"{THISDIR}/actualOutput_Diagonal-Star-17.vbr")
+	assert cmp == original
 
 
 @pytest.mark.skip("NotImplementedError")
-def test_DiagonalStar() -> None:
+def test_DiagonalStar_image() -> None:
 	"""Test diagonal star."""
-	dut.load(f"{THISDIR}/Diagonal-Star-17.vbr")
+	project.load(f"{THISDIR}/Diagonal-Star-17.vbr")
 	assert imgcompare.is_equal(
-		dut.image, f"{THISDIR}/desiredOutput_Diagonal-Star-17.png", tolerance=0.2
+		project.image, f"{THISDIR}/desiredOutput_Diagonal-Star-17.png", tolerance=0.2
 	)
-	# test round-trip compatibility
-	dut.save(f"{THISDIR}/actualOutput_Diagonal-Star-17.vbr")
-	original = GimpVbrBrush(f"{THISDIR}/Diagonal-Star-17.vbr")
-	assert dut == original
